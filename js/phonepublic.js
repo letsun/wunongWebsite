0697e2cb9daaad6145126a3896d3eea7fb062a77
huyao95 <huyao95@example.com>
2019-09-10 15:55:17 +0800

    物农官网

--- js/phonepublic.js
+++ js/phonepublic.js
@@ -1,11 +1,14 @@
 $(function () {
+    // debugger
     //点击开启侧边栏
     $('.header-img2').on('click', function () {
         $('.sidebar').fadeIn()
     })
 
     //关闭侧边栏
+
     $('.sidebar-cona').on('click', function () {
+        
         $('.sidebar').fadeOut()
     })
 
